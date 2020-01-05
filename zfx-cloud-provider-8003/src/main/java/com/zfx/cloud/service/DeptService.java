8a6a0733104bdbc7724a067c32aafdf102550a71
zfx1101804091 <1875586562@qq.com>
2020-01-05 20:58:33 +0800

    Ribbon负载均衡 ---轮询算法

---
+++ zfx-cloud-provider-8003/src/main/java/com/zfx/cloud/service/DeptService.java
@@ -0,0 +1,13 @@
+package com.zfx.cloud.service;
+
+import com.zfx.cloud.entities.Dept;
+
+import java.util.List;
+
+public interface DeptService {
+    public boolean add(Dept dept);
+
+    public Dept get(Long id);
+
+    public List<Dept> list();
+}
---
+++ zfx-cloud-provider-8003/src/main/java/com/zfx/cloud/service/impl/DeptServiceImpl.java
@@ -0,0 +1,37 @@
+package com.zfx.cloud.service.impl;
+
+import com.zfx.cloud.dao.DeptDao;
+import com.zfx.cloud.entities.Dept;
+import com.zfx.cloud.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @description:
+ * @author: zheng-fx
+ * @time: 2020/1/4 0004 13:40
+ */
+@Service
+public class DeptServiceImpl implements DeptService {
+    
+    @Autowired
+    private DeptDao deptDao;
+    
+    @Override
+    public boolean add(Dept dept) {
+        return deptDao.addDept(dept);
+    }
+
+    @Override
+    public Dept get(Long id) {
+        return deptDao.findById(id);
+    }
+
+    /*add,get,list 符合controller层的restful风格*/
+    @Override
+    public List<Dept> list() {
+        return deptDao.findAll();
+    }
+}
