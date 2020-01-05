2ebacab95759b85efffda28aa9624e3b14881c30
zfx1101804091 <1875586562@qq.com>
2020-01-05 22:06:31 +0800

    Ribbon负载均衡 ---自定义算法上

--- zfx-cloud-consumer-80/src/main/java/com/zfx/cloud/ZfxCloudConsumer80_App.java
+++ zfx-cloud-consumer-80/src/main/java/com/zfx/cloud/ZfxCloudConsumer80_App.java
@@ -1,8 +1,10 @@
 package com.zfx.cloud;
 
+import com.zfx.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * Hello world!
@@ -10,6 +12,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+//在启动该微服务的时候就去加载我们自定义的Ribbon配置类，从而使配置生效
+@RibbonClient(name = "ZFX-CLOUD-PROVIDER-8001",configuration = MySelfRule.class)
 public class ZfxCloudConsumer80_App 
 {
     public static void main( String[] args )
---
+++ zfx-cloud-consumer-80/src/main/java/com/zfx/myrule/MySelfRule.java
@@ -0,0 +1,20 @@
+package com.zfx.myrule;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @description: 自定义Ribbon规则类
+ * @author: zheng-fx
+ * @time: 2020/1/5 0005 21:59
+ */
+@Configuration
+public class MySelfRule {
+    
+    @Bean
+    public IRule myRule(){
+        return new RandomRule();//Ribbon默认时轮询，这里我自定义为随机
+    }
+}
